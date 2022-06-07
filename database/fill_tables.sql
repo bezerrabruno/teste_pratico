719e1f4193ccc2cc1a3918f3f702b8e6dc96cbf9
Bruno <bmb_bezerra@outlook.com>
2022-06-07 12:09:45 -0300

    Update Script DB

---
+++ database/fill_tables.sql
@@ -0,0 +1,47 @@
+INSERT INTO courses
+(
+	name,
+	description,
+	menu
+)
+VALUES
+(
+	'Flutter para iniciantes',
+	'Se torne o melhor com esse curso.',
+	'Aprenda o basico, persistencia de dado, gerencia de estado...'
+);
+
+INSERT INTO courses
+(
+	name,
+	description,
+	menu
+)
+VALUES
+(
+	'Flutter para intermediarios',
+	'Se torne o melhor dos melhores com esse curso.',
+	'Aprenda como usar o firebase...'
+);
+
+INSERT INTO students
+(
+	name,
+	age
+)
+VALUES
+(
+	'Bruno',
+	'21',
+);
+
+INSERT INTO students
+(
+	name,
+	age
+)
+VALUES
+(
+	'João',
+	'37',
+);
