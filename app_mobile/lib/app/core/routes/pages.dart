03d63881390270aae670788a72a3386c117cb945
Bruno <bmb_bezerra@outlook.com>
2022-06-07 03:45:01 -0300

    Update routes

--- app_mobile/lib/app/core/routes/pages.dart
+++ app_mobile/lib/app/core/routes/pages.dart
@@ -1,9 +1,11 @@
+import 'package:app_mobile/app/modules/students/students_binding.dart';
 import 'package:get/get.dart';
 
+import '/app/modules/splash/presenter/pages/splash_page.dart';
+import '/app/modules/splash/splash_binding.dart';
 import '/app/modules/home/home_binding.dart';
 import '/app/modules/home/presenter/pages/home_page.dart';
-import 'package:app_mobile/app/modules/splash/presenter/pages/splash_page.dart';
-import 'package:app_mobile/app/modules/splash/splash_binding.dart';
+import '/app/modules/courses/courses_binding.dart';
 
 part './routes.dart';
 
@@ -17,7 +19,11 @@ abstract class AppPages {
     GetPage(
       name: Routes.home,
       page: () => const HomePage(),
-      binding: HomeBinding(),
+      bindings: [
+        HomeBinding(),
+        CoursesBinding(),
+        StudentBinding(),
+      ],
     ),
   ];
 }
